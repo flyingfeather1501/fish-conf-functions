4030232a3df98f9bf1034f82bffe10073543a311
Moonlight Feather <flyingfeather1501@gmail.com>
2016-09-26 22:25:37 +0800

    fix mistyped path

--- conf-xfce4-session.fish
+++ conf-xfce4-session.fish
@@ -1,5 +1,5 @@
 function conf-xfce4-session
     read -p "echo EDITOR: " conf_editor
-    eval $conf_editor $HOME/.config/xfce4/xfconf/xfce4-session.conf
+    eval $conf_editor $HOME/.config/xfce4/xfconf/xfce-perchannel-xml/xfce4-session.xml
 end
 
